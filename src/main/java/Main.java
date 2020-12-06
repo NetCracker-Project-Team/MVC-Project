841cfd04891253534620e69a4c85b6ca12ee6243
mirai19 <61752311+mirai19@users.noreply.github.com>
2020-12-06 23:36:21 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Model.Dish dish1 = new Model.Dish("наполеон", desert, 150);
         Model.Dish dish2 = new Model.Dish("мороженое",desert,100);
         Model.Category drink = new Model.Category("напитки");
-        Model.Dish dish3 = new Model.Dish("сок",drink,200); ////
+        Model.Dish dish3 = new Model.Dish("сок",drink,200);
         //Лист блюд
         List<Model.Dish> dishes = List.of(dish1,dish2,dish3);
         //Сериализация  десериализация
