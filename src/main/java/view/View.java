c61071d60db26ecae5800742aaccfedaf69f1b75
mirai19 <61752311+mirai19@users.noreply.github.com>
2020-12-11 19:04:00 +0400

    Update View.java

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -240,6 +240,239 @@ public class View {
         if (in.next().equals("1")) newDish();
         else  mainMenu();
     }
+/**
+ * Класс - представление
+ */
+public class View {
+
+    /** Поле для сканнера */
+    private static Scanner in = new Scanner(System.in);
+    /** Поле для правила выбора пунктов меню */
+    private static String selectionRule = " *выберите пункт меню, чтобы вы хотели сделать";
+    /** Поле для выбора пункта меню пользователем */
+    private static String selection;
+    /** Поле для неправильного выбора пункта меню */
+    private static String wrongChoice = "\n\t!!Вы неправильно выбрали пункт меню!!";
+    /** Поле для пустого меню */
+    private static String emptyMenu = "\n\tВы не можете выполнить операцию. Так как меню пустое!\n" +
+                                      "\tПопробуйте снова.";
+    /** Поле для файла блюд */
+    private static File file;
+    /** Поле для файла категорий*/
+    private static File fileCategory ;
+
+
+    /**
+    *Метод главного меню
+     */
+    public static void mainMenu(){
+        System.out.println(selectionRule);
+        System.out.println("\n\t\t---Главное меню:---");
+        System.out.println("\t1 - Загрузить меню ресторана\n" +
+                           "\t2 - Сохранить меню ресторана\n" +
+                           "\t3 - Посмотреть данные\n" +
+                           "\t4 - Добавление новых данных\n" +
+                           "\t5 - Редактировать меню ресторана\n " +
+                           "\t6 - Поиск данных в ресторане\n" +
+                           "\t7 - Выйти из программы" );
+
+        System.out.print("\n---Ваш выбор:");
+        selection = in.next();
+        switch(selection){
+            case "1":
+                download(0);
+                break;
+            case "2":
+                if (file == null || file.length() == 0){
+                    System.out.println(emptyMenu);
+                    mainMenu();
+                }
+                else{
+                    save();
+                }
+                break;
+            case "3":
+                if (file == null || file.length() == 0){
+                    System.out.println(emptyMenu);
+                    mainMenu();
+                }
+                else{
+                    showMenu();
+                }
+                break;
+            case "4":
+                if (file == null){
+                    System.out.println("\n\tНет файла!");
+                    mainMenu();
+                }
+                else{
+                    newDish();
+                }
+                break;
+            case "5":
+                if (file == null || file.length() == 0){
+                    System.out.println(emptyMenu);
+                    mainMenu();
+                }
+                else{
+                    editMenu();
+                }
+                break;
+            case "6":
+                if (file == null || file.length() == 0){
+                    System.out.println(emptyMenu);
+                    mainMenu();
+                }
+                else{
+                    search();
+                }
+                break;
+            case "7":
+                return;
+            default:
+                System.out.println(wrongChoice);
+                mainMenu();
+        }
+    }
+
+    /**
+     *Метод для загрузки меню ресторана
+     * @throws IOException
+     */
+    private static void download(int k){
+        System.out.println("\n\t\t---Загрузка меню ресторона---");
+        try {
+            System.out.print("\nПуть к файлу для меню: ");
+            file = new File(in.next());
+            fileCategory = new File("fileC.json");
+            fileCategory.deleteOnExit();
+            Controller.addCategoryByDish(file,fileCategory);
+            System.out.println("\t\tЗагрузка выполнена успешно!\n" );
+        }
+        catch (Exception e){
+            System.out.println("\t\t"+e.toString()+"\n");
+        }
+        if (k == 0) {
+            System.out.println("\t1 - попробовать снова\n" +
+                    "\tлюбая другая последовательность - вернуться в главное меню");
+            selection = in.next();
+            if (selection.equals("1")) {
+                download(k);
+            } else {
+                mainMenu();
+            }
+        }
+
+    }
+
+    /**
+     *Метод для сохранения меню ресторона в выбранный файл
+     *@throws IOException
+     */
+    private static void save(){
+        System.out.println("\n\t\t---Сохранение меню ресторона---");
+        System.out.print("\nПуть к файлу: ");
+        String nameFile = in.next();
+        File file0 = new File(nameFile);
+        try {
+            Serialize.serialize(Serialize.deserialize(file),file0);
+            System.out.println("\t\tСохранение выполнено успешно!\n" +
+                    "\tлюбая другая последовательность - вернуться в главное меню");
+            System.out.print("\n---Ваш выбор: ");
+            selection = in.next();
+            mainMenu();
+        }
+        catch(IOException e) {
+            System.out.println("\t\tЧто-то пошло не так при сохранении...:(\n" +
+                    "\t1 - попробовать снова\n" +
+                    "\tлюбая другая последовательность - вернуться в главное меню");
+            System.out.print("\n---Ваш выбор: ");
+            selection = in.next();
+            if (selection.equals("1")){
+                save();
+            }
+            else{
+                mainMenu();
+            }
+        }
+    }
+
+    /**
+     *Метод просмотра меню ресторана
+     */
+    private static void showMenu(){
+        System.out.println("\n\t\tПросмотр данных:\n" +
+                "\t1 - меню ресторана\n" +
+                "\t2 - блюда по категории\n" +
+                "\t3 - категории ресторана\n" +
+                "\t другая последовательность - выход в меню ");
+        System.out.print("\n---Ваш выбор: ");
+        selection = in.next();
+        switch (selection){
+            case "1":
+                System.out.println(Controller.print(file));
+                break;
+            case "2":
+                System.out.println(Controller.print(inputName("Название категории: ",1),file));
+                break;
+            case "3":
+                System.out.println(Controller.printCategory(fileCategory));
+                break;
+            default:
+                mainMenu();
+        }
+        System.out.print("\n\tВведите любое значение, чтобы выйти в главное меню - ");
+        in.next();
+        mainMenu();
+    }
+
+    /**
+     *Метод добавления блюда
+     */
+    private static void newDish(){
+        System.out.println("\n\t\tДобавление данных:\n" +
+                "\t1 - добавить блюдо\n" +
+                "\t2 - добавить категорию\n" +
+                "\t3 - добавить данные из файла\n" +
+                "\t другая последовательность - выход в меню ");
+        System.out.print("\n---Ваш выбор: ");
+        selection = in.next();
+        switch (selection){
+            case "1": {
+                System.out.println("\n\t\tДобавление блюда");
+                int k = 2;
+                if (file.length() == 0) k = 5;
+                String name = inputName("Название блюда: ", k);
+                double price = inputPrice();
+                Category category = new Category(inputName("Категория блюда: ", 1));
+                Dish dish5 = new Dish(name, category, price);
+                Controller.addData(dish5, file, fileCategory);
+                break;
+            }
+            case "2": {
+                String nameC = inputName("Название категории: ", 1);
+                Controller.addCategory(new Category(nameC), fileCategory);
+                break;
+            }
+            case "3": {
+                System.out.print("Имя файла: ");
+                File file1 = new File(in.next());
+                if (!Controller.addFile(file, file1)) {
+                    System.out.println("\n\tЧто-то пошло не так!");
+                    editMenu();
+                    return;
+                }
+                break;
+            }
+            default:
+                mainMenu();
+        }
+        System.out.println("\n\t\tОперация успешно выполнена!");
+        System.out.println("\n\t\tХотите повторить?\n\t1 - да\n\tлюбая другая последовательность - вернуться в меню");
+        System.out.print("\n---Ваш выбор: ");
+        if (in.next().equals("1")) newDish();
+        else  mainMenu();
+    }
 
     /**
      *Метод редактирования меню ресторана
@@ -307,6 +540,8 @@ public class View {
             }
         }
         System.out.println("\n\t\tОперация успешно выполнена!");
+        System.out.println("\n\t\tИзменное меню");
+        Controller.print(file);
         System.out.println("\n\t\tХотите продолжить редактирование?\n\t1 - да\n\tлюбая клавиша - вернуться в меню");
         System.out.print("\n---Ваш выбор: ");
         if (in.next().equals("1")) editMenu();
